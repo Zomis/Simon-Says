ac0d9f23ea6c9396b73ab792fb27cf4b776d5a60
Simon Forsberg <zomis2k@hotmail.com>
2014-12-16 16:10:36 +0100

    setting up batch, camera, stage and skin

--- core/src/net/zomis/simon/SimonGame.java
+++ core/src/net/zomis/simon/SimonGame.java
@@ -1,19 +1,30 @@
 package net.zomis.simon;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Camera;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.utils.viewport.FitViewport;
 
-public class SimonGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
+public class SimonGame extends Game {
+	private SpriteBatch batch;
+    private Camera camera;
+    private Stage stage;
+    private Skin skin;
+
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+        camera = new OrthographicCamera(800, 480);
+        stage = new Stage(new FitViewport(800, 480, camera), batch);
+        skin = new Skin(Gdx.files.internal("data/uiskin.json"));
+        Gdx.input.setInputProcessor(stage);
 	}
 
 	@Override
@@ -21,7 +32,9 @@ public class SimonGame extends ApplicationAdapter {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+        super.render();
+        batch.end();
+        stage.act(Math.min(Gdx.graphics.getDeltaTime(), 1 / 30f));
+        stage.draw();
 	}
 }
